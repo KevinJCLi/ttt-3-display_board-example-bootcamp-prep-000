71651c776f3e11dc27466b5a39ecaee97907ef2d
Kevin Li <kevinjcli@hotmail.com>
2018-02-23 02:42:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 
 def display_board 
   tic_or_tac = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ",]
-  put "#{tic_or_tac[0]}|#{tic_or_tac[1]}|#{tic_or_tac[2]}"
-  put "-----------"
-  put "#{tic_or_tac[3]}|#{tic_or_tac[4]}|#{tic_or_tac[5]}"
-  put "-----------"
-  put "#{tic_or_tac[6]}|#{tic_or_tac[7]}|#{tic_or_tac[8]}"
+  puts "#{tic_or_tac[0]}|#{tic_or_tac[1]}|#{tic_or_tac[2]}"
+  puts "-----------"
+  puts "#{tic_or_tac[3]}|#{tic_or_tac[4]}|#{tic_or_tac[5]}"
+  puts "-----------"
+  puts "#{tic_or_tac[6]}|#{tic_or_tac[7]}|#{tic_or_tac[8]}"
 end
